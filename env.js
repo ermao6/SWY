166074f27877a0f07bb011443e1de85c8cd8342b
ermao6 <luermaoa@gmail.com>
2024-08-29 19:34:00 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '三文鱼',
+  // 站点描述
+  description: '不收集用户隐私，不留存任何日志记录。让您的上网体验真正做到私密、安全、无痕。',
+  // API
+  host: 'https://api.008228.xyz',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  // crisp
+  crisp_id: 'c6c4d628-98b4-4bba-9554-428edbb79d9d',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
